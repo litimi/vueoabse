96c4385864333119219246e35fc39cdde6a76b82
lxs123456 <1611830402@qq.com>
2020-03-24 10:23:38 +0800

    创建页面文件夹及文件

---
+++ oabase/src/views/home/index.vue
@@ -0,0 +1,19 @@
+<!--首页-->
+<template>
+  <div class="home">
+    1111111111111111111
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>>
