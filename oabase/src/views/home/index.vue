b653149cd00d58899d03fd04b2700ef8ab742300
lxs123456 <1611830402@qq.com>
2020-04-10 17:09:22 +0800

    首页面

--- oabase/src/views/home/index.vue
+++ oabase/src/views/home/index.vue
@@ -1,7 +1,7 @@
 <!--首页-->
 <template>
-  <div class="home">
-    1111111111111111111
+  <div class="oabase_back">
+
   </div>
 </template>
 
@@ -10,7 +10,33 @@
 export default {
   name: 'Home',
   components: {
-  }
+  },
+  data(){
+    return {
+      inpContent: ''
+    }
+  },
+  methods: {
+    //获取数据
+    getArticle() {
+      this.$http.get('/api/getValue', {
+        params: {id: 1}
+      }).then( (res) => {
+        console.log('res', res);
+        this.inpContent = res.data[0].name;
+      })
+    },
+
+    // 添加数据
+    insertValue(){
+      this.$http.post('/api/insertValue', {
+        name: '测试'
+      }).then( (res) => {
+        console.log('res', res);
+        // this.inpContent = res.data[0].name;
+      })
+    }
+  },
 }
 </script>
 
