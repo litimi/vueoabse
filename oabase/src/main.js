be928d8c18c4cdad92696c732e3da27b1a69b1d6
lxs123456 <1611830402@qq.com>
2020-04-10 17:11:18 +0800

    引入axios，引入公用样式文件

--- oabase/src/main.js
+++ oabase/src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/global.css'
 import './mock.js'
 
 Vue.config.productionTip = false
-
 Vue.use(ElementUI);
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
