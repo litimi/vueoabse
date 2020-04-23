1e91cdabf15fe07f60dea01b0bf8fb8c4c825701
lxs123456 <1611830402@qq.com>
2020-04-23 16:49:28 +0800

    面包屑控件添加

---
+++ oabase/src/components/Bread/index.vue
@@ -0,0 +1,27 @@
+<!--面包屑-->
+<template>
+  <div class="blog_bread">
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Bread',
+    components: {
+    },
+    data(){
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>>
